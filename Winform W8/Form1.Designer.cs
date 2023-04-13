022cb7dbf6035aca8219fd286a574fec0988ec7e
feliciastevany <128245119+feliciastevany@users.noreply.github.com>
2023-04-13 13:24:31 +0700

    Add files via upload

---
+++ Winform W8/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Winform_W8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPemain = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPemain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPemain
+            // 
+            this.dgvPemain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPemain.Location = new System.Drawing.Point(7, 88);
+            this.dgvPemain.Name = "dgvPemain";
+            this.dgvPemain.Size = new System.Drawing.Size(787, 354);
+            this.dgvPemain.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(100, 43);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(37, 13);
+            this.labelValue.TabIndex = 2;
+            this.labelValue.Text = "HEHE";
+            this.labelValue.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Value:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(381, 6);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dgvPemain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPemain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPemain;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Winform W8/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Winform_W8
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection mysqlConnect;
+        MySqlCommand mysqlCommand; //code yg ada di mysql (select*form)
+        MySqlDataAdapter mySqlAdapter; // menerima hasil dari query// hasil dari selection
+        string connectionString;
+        string sqlQuery;
+        DataTable dataPemain = new DataTable();
+        DataTable dataNationality = new DataTable();
+        DataTable dataTeamname = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = "server=localhost;uid=root;pwd=;database=premier_league";
+            mysqlConnect = new MySqlConnection(connectionString);
+            // mysqlConnect.Open(); // hanya digunakan ketika menggunakan DML (merubah data)
+
+            sqlQuery = "SELECT player_id as 'Player ID', player_name as 'Player Name', height as 'Player Height', team_name as 'Team name'\r\nFROM player INNER JOIN team\r\nON player.team_id = team.team_id\r\n;";
+            mysqlCommand = new MySqlCommand(sqlQuery, mysqlConnect);
+            mySqlAdapter = new MySqlDataAdapter(mysqlCommand);
+            mySqlAdapter.Fill(dataPemain);
+            dgvPemain.DataSource = dataPemain;
+
+            sqlQuery = "SELECT nationality_id as 'nationality ID', nation as 'nation' FROM nationality n";
+            mysqlCommand = new MySqlCommand(sqlQuery, mysqlConnect);
+            mySqlAdapter = new MySqlDataAdapter(mysqlCommand);
+            mySqlAdapter.Fill(dataNationality);
+            comboBox1.DataSource = dataNationality;
+            comboBox1.ValueMember = "nationality ID";
+            comboBox1.DisplayMember = "nation";
+
+            sqlQuery = "SELECT team_name as 'Team Name' FROM team";
+            mysqlCommand = new MySqlCommand(sqlQuery, mysqlConnect);
+            mySqlAdapter = new MySqlDataAdapter(mysqlCommand);
+            mySqlAdapter.Fill(dataTeamname);
+            comboBox2.DataSource = dataTeamname;
+            comboBox2.DisplayMember = "Team Name";
+            comboBox2.ValueMember = "Team Name";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            labelValue.Text = comboBox1.SelectedValue.ToString();
+
+            //dgvPemain = new DataTable();
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataTeamname = new DataTable();
+            sqlQuery = "SELECT player_id as 'Player ID', player_name as 'Player Name', height as 'Player Height', team_name as 'Team name'\r\nFROM player INNER JOIN team\r\nON player.team_id = team.team_id\r\nWHERE team_name = '" + comboBox2.SelectedValue.ToString() + "';";
+            mysqlCommand = new MySqlCommand(sqlQuery, mysqlConnect);
+            mySqlAdapter = new MySqlDataAdapter(mysqlCommand);
+            mySqlAdapter.Fill(dataTeamname);
+            dgvPemain.DataSource = dataTeamname;
+        }
+    }
+}
